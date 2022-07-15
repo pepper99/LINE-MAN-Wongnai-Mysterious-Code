30b81ed466f673f19ca16deda2a70abd9b55bd5c
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-07-15 23:13:15 +0700

    Add quotation marks

--- sol.py
+++ sol.py
@@ -3,4 +3,4 @@ from base64 import b64decode
 if __name__ == '__main__':
     secret = 'aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K'
     ans = b64decode(secret).decode('utf-8')[::-1]
-    print(f'The answer is {ans}')
+    print(f'The answer is "{ans}"')
