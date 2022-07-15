a82c8a09ecf650188103295f4ff6bcad9cb7d238
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-07-15 23:09:11 +0700

    Add solution

---
+++ sol.py
@@ -0,0 +1,6 @@
+from base64 import b64decode
+
+if __name__ == '__main__':
+    secret = 'aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K'
+    ans = b64decode(secret).decode('utf-8')[::-1]
+    print(f'The answer is {ans}')
